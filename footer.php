d4ff589642e40a0bc2f56326c83a6ac53494253e
Kadir Mutlu <kadirmutluu@gmail.com>
2016-09-22 15:05:09 +0300

    Wordpress çatısı oluşturuldu. header.php düzenlendi.

---
+++ footer.php
@@ -0,0 +1,33 @@
+<footer>
+<div class="container">
+	<div class="row">
+  		<div class="col-md-4"><br />
+        <span class="fbaslik">SOSYAL MEDYADA BİZ</span><div class="cizgi"></div>
+        <a href="#"><img class="sosyal" src="images/64-facebook.png" /></a>
+        <a href="#"><img class="sosyal" src="images/64-twitter.png" /></a>
+        <a href="#"><img class="sosyal" src="images/64-youtube-2.png" /></a><br />
+        <a href="#"><img class="sosyal" src="images/64-instagram.png" /></a>
+        <a href="#"><img class="sosyal" src="images/64-googleplus.png" /></a>
+        <a href="#"><img class="sosyal" src="images/64-rss.png" /></a><br /><br />
+      </div>
+  		<div class="col-md-4"><br />
+        <span class="fbaslik">SAYFALAR</span><div class="cizgi"></div>
+        <ul class="altmenu">
+          <li><a href="#">Anasayfa</a></li>
+          <li><a href="#">İletişim</a></li>
+          <li><a href="#">Hakkımızda</a></li>
+          <li><a href="#">Yazarlar</a></li>
+        </ul>
+      </div>
+  		<div class="col-md-4"><br />
+        <span class="fbaslik">BİLGİLENDİRME</span><div class="cizgi"></div>
+        <p class="altyazi">Bu sitede yer alan tüm içerikler diziger.com'a aittir. Diziger.com'un izni olmaksızın başka yerde yayınlanamaz. Eğer alıntı yapmak isterseniz lütfen kaynak gösteriniz.</p>
+        <p class="copy">Copyright &copy; 2016 diziger.com Tüm hakları saklıdır.</p>
+      </div>
+	</div>
+</div>
+</footer>
+    <script src="js/jquery-3.1.0.slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>    
+</body>
+</html>
