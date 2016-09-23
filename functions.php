37313e8bf173f52ebceb35b9151624fecdb87f21
Kadir Mutlu <kadirmutluu@gmail.com>
2016-09-23 16:57:09 +0300

    index.php wordpress'e entegre edildi.

--- functions.php
+++ functions.php
@@ -1,14 +1,11 @@
 <?php
-add_theme_support('menus');
-register_nav_menus(array(
- 'menu' => 'Header Menü'
-));
-?>
-<?php
 // Register Custom Navigation Walker
 require_once('wp_bootstrap_navwalker.php');
 
 register_nav_menus( array(
     'primary' => __( 'Primary Menu', 'diziger' ),
 ) );
+
+add_theme_support( 'post-thumbnails' );
+add_image_size( 'bizim', 1366, 768, array('left', 'top'), true );
 ?>
